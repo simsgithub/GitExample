8148c354c73fb4aeae627bb04794a4d7dca9cc98
Sim <sims@verizon.net>
2016-07-19 12:31:19 -0400

    Comment Updated

--- GitExample/ViewController.swift
+++ GitExample/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // extra comment!
+        // extra comment!!
     }
 
     override func didReceiveMemoryWarning() {
